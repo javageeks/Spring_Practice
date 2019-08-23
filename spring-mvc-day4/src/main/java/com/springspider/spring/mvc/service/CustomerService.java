4b93bc12aec269e9ca8f43692c6d5f7255ee5f37
Shaik Khaja Hussain <hussainshaik18@gmail.com>
2019-08-23 16:23:18 +0530

    Spring mvc + JPA + MySql

---
+++ spring-mvc-day4/src/main/java/com/springspider/spring/mvc/service/CustomerService.java
@@ -0,0 +1,37 @@
+package com.springspider.spring.mvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springspider.spring.mvc.dao.CustomerRepository;
+import com.springspider.spring.mvc.model.Customer;
+
+@Service("customerservice")
+@Transactional
+public class CustomerService {
+	
+	@Autowired CustomerRepository repo;
+	
+	public void save(Customer customer) {
+		repo.save(customer);
+	}
+	
+	public List<Customer> listAll() {
+		return (List<Customer>) repo.findAll();
+	}
+	
+	public Customer get(Long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+	
+	public List<Customer> search(String keyword) {
+		return repo.search(keyword);
+	}
+}
